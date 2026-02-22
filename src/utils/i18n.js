d811b9ad72e9b5277ff00b9d65af642623b26ce7
stanley <stanleygomesdasilva@gmail.com>
2026-02-22 15:42:20 -0300

    chore: move to inside folder

--- src/utils/i18n.js
+++
@@ -1,8 +0,0 @@
-const i18n = require('i18n')
-const config = require('../config')
-const i18nConfig = config.i18n
-
-i18n.configure(i18nConfig)
-i18n.translate = (text, ...parameters) => i18n.__(text, ...parameters)
-
-module.exports = i18n
